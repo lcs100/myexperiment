a07c6e7993629fdc0e911ca4b1d6981daf8497f7
liuchenshuo <liuchenshuo@intra.nsfocus.com>
2020-12-28 20:46:37 +0800

    test

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
 import path
 
+initial_difficulty =
+
+node_number = 100
+first_round_d = 
+second_round_d = 
+runner_up_list = []
+total_cycle = 1000
+
 
 
 
 
 
 if __init__ == '__main__':
-    
+    xw
